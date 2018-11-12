66991a04da113ee92033b9e9947b7d4736dde3a0
aminur rahman <aminuriub135@gmail.com>
2018-11-12 03:24:44 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var http = require('http');
+var exports=require('express');
+var app=express();
+var server=http.server(app);
+
+app.get('/',function(request,response){
+  response.sendFile(_dirname+'/index.html');
+});
+app.get('/about',function(request,response){
+  response.sendFile(_dirname+'/about.html');
+});
+
+/*var fs =require('fs');
   var server = http.createServer(function(req, res){
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World\n");
-  });
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('node.html',function(err,data){
+       if(err){
+           return console.log("File read error")
+       } 
+       res.end(data);
+    })
+    //res.end("Hello World\n");
+  });*/
   server.listen(process.env.PORT ||3000 , process.env.IP, function(){
     console.log('Server running');
   });
